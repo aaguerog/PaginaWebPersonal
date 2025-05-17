c01794013c58878442d90922996c03d8da2ee29d
aaguerog <159979024+aaguerog@users.noreply.github.com>
2025-05-16 20:30:26 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     emailjs.init("9Y0-cdx40yJ3Qt4Jw");
 
-    const form = document.getElementById("formulario");
+    const form = document.querySelector("form");
 
     form.addEventListener("submit", function (event) {
-        event.preventDefault();
+        event.preventDefault(); 
 
         emailjs.sendForm("service_hr64j0r", "template_wvxg69c", form)
             .then(function () {
                 Swal.fire({
-                icon: 'success',
-                title: '¡Mensaje enviado!',
-                text: 'Gracias por contactarme. Te responderé pronto.',
-                confirmButtonColor: '#006494'
+                    icon: 'success',
+                    title: '¡Mensaje enviado!',
+                    text: 'Gracias por contactarme. Te responderé pronto.',
+                    confirmButtonColor: '#006494'
                 });
                 form.reset();
             }, function (error) {
                 Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Ocurrió un error al enviar el mensaje. Intenta nuevamente.',
-                confirmButtonColor: '#006494'
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Ocurrió un error al enviar el mensaje. Intenta nuevamente.',
+                    confirmButtonColor: '#006494'
                 });
+                console.error("Error al enviar:", error);
             });
     });
 });
