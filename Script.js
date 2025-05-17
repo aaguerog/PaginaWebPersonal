362276169361bc65993e8b6f14372a7aba7a6fba
aaguerog <159979024+aaguerog@users.noreply.github.com>
2025-05-16 20:16:59 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     emailjs.init("9Y0-cdx40yJ3Qt4Jw");
 
-    const form = document.querySelector("form");
+    const form = document.getElementById("formulario");
 
     form.addEventListener("submit", function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
 
         emailjs.sendForm("service_hr64j0r", "template_wvxg69c", form)
             .then(function () {
                 alert("¡Mensaje enviado con éxito!");
-                form.reset(); 
+                form.reset();
             }, function (error) {
                 console.error("Error al enviar:", error);
                 alert("Ocurrió un error. Inténtalo de nuevo.");
